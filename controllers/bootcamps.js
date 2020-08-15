f4e528659677b19ddbfca4370c9ef50cafac2507
Hyung Kim <hjkmines@gmail.com>
2020-08-14 21:06:45 -0600

    adds geocode

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,5 +1,6 @@
 const Bootcamp = require('../models/Bootcamp')
 const ErrorResponse = require('../middleware/errorResponse')
+const geocoder = require('../utills/geocoder')
 const asyncHandler = require('../middleware/async')
 
 //description: Get all bootcamps 
@@ -68,4 +69,31 @@ exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
         }
     res.status(200).json({success: true, data: {} })
 
+})
+
+//description: Get bootcamps within a radius 
+//route: Delete /api/v1/bootcamps/radius/:zipcode/:distance 
+//access: private 
+exports.getBootcampsInRadius = asyncHandler(async (req, res, next) => {
+    const { zipcode, distance } = req.params; 
+
+    //Get lat/lng from geocoder 
+    const loc = await geocoder.geocode(zipcode); 
+    const lat = loc[0].latitude
+    const lng = loc[0].longitude
+
+    //calc radius using radians 
+    //Divide dist by radius of Earth 
+    //Earth radius = 3,963 mi / 6,378 km 
+    const radius = distance / 3963; 
+
+    const bootcamps = await Bootcamp.find({
+        location: {$geoWithin: { $centerSphere: [[lng, lat], radius]}}
+    })
+
+    res.status(200).json({
+        success: true, 
+        count: bootcamps.length, 
+        data: bootcamps
+    })
 })
