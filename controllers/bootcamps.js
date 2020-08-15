60830283f036536e9356be80092113f421d2e50f
Hyung Kim <hjkmines@gmail.com>
2020-08-15 11:53:02 -0600

    adds upload

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -179,4 +179,19 @@ exports.bootcampPhotoUpload = asyncHandler(async (req, res, next) => {
             new ErrorResponse(`Please upload a file`, 400)
         )
     }
+
+    const file = req.files.file; 
+
+    //Make sure the image is a photo 
+    if(!file.mimetype.startsWith('image')) {
+        return next(new ErrorResponse(`Please upload an image file`, 400))
+    }
+
+    //Check filesize 
+    if(file.size > process.env.MAX_FILE_UPLOAD) {
+        return next(new ErrorResponse(`Please upload an image less than ${process.env.MAX_FILE_UPLOAD}`, 400))
+    }
+
+    //Create custom file name
+    file.name =`photo_${bootcamp._id}`
 })
